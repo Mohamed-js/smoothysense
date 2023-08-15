8cb392c491eb952f3aa9799189d72567e5385b0e
Mohamed Atef <atefhamosa@gmail.com>
2023-08-16 01:54:29 +0300

    Add GTM

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,6 +6,8 @@ import { Provider } from "react-redux";
 import { Readex_Pro } from "next/font/google";
 import { appWithTranslation } from "next-i18next";
 import Script from "next/script";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 const changa = Readex_Pro({
   weight: ["400", "500", "600", "700"],
@@ -13,20 +15,12 @@ const changa = Readex_Pro({
 });
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-TXXM49JZ" });
+  }, []);
   return (
     <Provider store={store}>
       <Head>
-        <Script
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TXXM49JZ');`,
-          }}
-        ></Script>
         <title>SMOOTHYSENSE</title>
         <link rel="shortcut icon" href="/logoo2.png" type="image/x-icon" />
       </Head>
